fa88e62f04e5ecd01c83c32a4f086766ff555150
Stefan Wojcik <stefan.wojcik@colorado.edu>
2016-02-22 22:09:21 -0500

    Added files via upload

--- data_building/createSeedFeature.R
+++ data_building/createSeedFeature.R
@@ -1,11 +1,15 @@
 #script to make the seeds feature
 library(caret)
 
-setwd("/Volumes/TINY CRYPT/papers/Personal/NCAA/2016 Kaggle Data/march-machine-learning-mania-2016-v1")
-
-seeds <- read.csv("TourneySeeds.csv")
+#setwd("/Volumes/TINY CRYPT/papers/Personal/NCAA/2016 Kaggle Data/march-machine-learning-mania-2016-v1")
+#seeds <- read.csv("TourneySeeds.csv")
 
 #Make seed
-seeds$Seed <- as.numeric(gsub("[aA-zZ]", "", seeds$Seed))
+makeSeedFeature <- function(seeds.file){
+  seeds <- read.csv(seeds.file)
+  seeds$Seed <- as.numeric(gsub("[aA-zZ]", "", seeds$Seed))  
+  return(seeds)
+}
 
-write.csv(seeds, "TourneySeeds_feature.csv", row.names=F)
+
+#write.csv(seeds, "TourneySeeds_feature.csv", row.names=F)
